ba387e252687b614d49e13d1ead1c232d7de8d2a
Vika <isaenko1990@mail.ru>
2022-11-28 13:31:02 +0600

    Добавлен код программы

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите несколько значений через пробел: ");
+string[] input = Console.ReadLine().Split();
+string[] result = new string[input.Length];
+
+int index = 0;
+
+foreach(var item in input ) {
+  if(item.Length <= 3) {
+    result[index] = item;
+    index++;
+  }
+}
+
+if(result.Length < 1) {
+  Console.WriteLine("Не найдено ни одного результата.");
+} else {
+  result = result.Where(
+    item => !string.IsNullOrEmpty(item)
+  ).ToArray();
+  
+  string output = string.Join(", ", result);
+  Console.WriteLine($"Результат: {output}");
+}
